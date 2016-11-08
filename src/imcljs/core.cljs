4d6c9d5a368b18d0987126e16e1e96b115b57d05
Joshua Heimbach <joshkh@gmail.com>
2016-11-08 17:44:11 +0000

    init

---
+++ src/imcljs/core.cljs
@@ -0,0 +1,18 @@
+(ns imcljs.core
+  (:require [imcljs.io :as io] ))
+
+(enable-console-print!)
+
+
+
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  (.log js/console "d" (io/path {:root "www.flymine.org/query"} "/something"))
+)
