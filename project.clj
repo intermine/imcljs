28f1f859990a95d4bf4222fea549748120c091b1
Joshua Heimbach <joshkh@gmail.com>
2018-03-07 14:52:42 +0000

    Adds support for code generation

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.intermine/imcljs "0.4.0"
+(defproject org.intermine/imcljs "0.5.0"
   :description "imcljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
