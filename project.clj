39aa9c7fdc5ed01da59752e180627d3a6c7ed51c
Joshua Heimbach <joshkh@gmail.com>
2018-03-16 12:19:38 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.intermine/imcljs "0.5.1"
+(defproject org.intermine/imcljs "0.6.0-SNAPSHOT"
   :description "imcljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
