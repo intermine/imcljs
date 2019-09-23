937edffecf7436b4f8182606fa63b1c424375fe9
uosl <regen@anche.no>
2019-09-23 17:36:09 +0100

    Fix codox by giving it a source-paths of only src

--- project.clj
+++ project.clj
@@ -14,13 +14,15 @@
                  [clj-http "3.10.0"]
                  [cheshire "5.8.1"]
                  [aysylu/loom "1.0.2"]]
-  :codox {:language :clojurescript}
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
-            [lein-doo "0.1.10"]
+            [org.clojure/core.unify "0.5.7"]
             [lein-cljfmt "0.6.1"]
-            [lein-codox "0.10.5"]]
+            [lein-codox "0.10.7"]]
 
   :source-paths ["src/cljc" "src/cljs" "src/clj" "test/clj" "test/cljs"]
+
+  :codox {:language :clojurescript
+          :source-paths ["src"]}
 
   :figwheel {:server-port 5003
              :reload-clj-files {:clj true :cljc true}}
@@ -61,8 +63,9 @@
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [figwheel-sidecar "0.5.19"]
                                   [cider/piggieback "0.4.1"]]
-                   :plugins [[lein-figwheel "0.5.19"]]
-                   :source-paths ["src" "dev"]}
+                   :plugins [[lein-figwheel "0.5.19"]
+                             [lein-doo "0.1.10"]]}
+             :repl {:source-paths ["dev"]}
              :java9 {:jvm-opts ["--add-modules" "java.xml.bind"]}}
 
   :cljfmt {:indents {async [[:inner 0]]}})
