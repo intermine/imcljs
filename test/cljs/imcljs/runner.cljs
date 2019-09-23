ea312920bbbd80a939a0cfbd4b2d86f221867425
uosl <regen@anche.no>
2019-09-23 14:14:06 +0100

    Informative assertions for restful arguments
    Resolves #34. This gives useful error messages when request data is
    missing or invalid, instead of a random failing Clojure function
    throwing.

--- test/cljs/imcljs/runner.cljs
+++ test/cljs/imcljs/runner.cljs
@@ -6,6 +6,7 @@
             [imcljs.list-test]
             [imcljs.assets-test]
             [imcljs.registry-test]
-            [imcljs.auth-test]))
+            [imcljs.auth-test]
+            [imcljs.utils-test]))
 
-(doo-tests 'imcljs.path-test 'imcljs.query-test 'imcljs.list-test 'imcljs.assets-test 'imcljs.registry-test 'imcljs.auth-test)
+(doo-tests 'imcljs.path-test 'imcljs.query-test 'imcljs.list-test 'imcljs.assets-test 'imcljs.registry-test 'imcljs.auth-test 'imcljs.utils-test)
