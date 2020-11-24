a5ac1d219dd85eef5120e175382afd76a6d71052
uosl <regen@anche.no>
2020-11-24 16:15:56 +0000

    Tests for bg-properties ws

--- test/cljs/imcljs/runner.cljs
+++ test/cljs/imcljs/runner.cljs
@@ -9,6 +9,7 @@
             [imcljs.auth-test]
             [imcljs.utils-test]
             [imcljs.preferences-test]
-            [imcljs.saved-queries-test]))
+            [imcljs.saved-queries-test]
+            [imcljs.bgproperties-test]))
 
-(doo-tests 'imcljs.core-test 'imcljs.path-test 'imcljs.query-test 'imcljs.list-test 'imcljs.assets-test 'imcljs.registry-test 'imcljs.auth-test 'imcljs.utils-test 'imcljs.preferences-test 'imcljs.saved-queries-test)
+(doo-tests 'imcljs.core-test 'imcljs.path-test 'imcljs.query-test 'imcljs.list-test 'imcljs.assets-test 'imcljs.registry-test 'imcljs.auth-test 'imcljs.utils-test 'imcljs.preferences-test 'imcljs.saved-queries-test 'imcljs.bgproperties-test)
